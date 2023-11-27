4d7adb3daef0d9628b1938262ec79fc15429da44
Marco <80124925+PalawaJoko@users.noreply.github.com>
2023-11-27 20:24:33 -0300

    Update Main.c
    Se añade codigo util

--- Main.c
+++ Main.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(){
-  printf("Hola Mundo")
+	printf("Bienvenido a Sopa de Letras 1.0\n");
+	char opcion;
+	do{
+		printf("Por favor, Elija una opcion:\n1-Jugar\n2-Ver Puntajes\n3-.Salir\n");
+		scanf("%s",&opcion);
+		switch(opcion){
+		case '1':
+			printf("Entre a la opcion 1\n");
+			break;
+		case '2':
+			printf("Entre a la opcion 2\n");
+			break;
+		case '3':
+			printf("Terminando programa, gracias por entrar a Sopa de Letras 1.0\n");
+			break;
+		default:
+			printf("opcion no valida\n\n");
+		}
+	}while(opcion!='3');
+	return 0;
 }
