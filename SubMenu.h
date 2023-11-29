c73b06b437d0203229bc7ae504f3d9d47b8ee214
Marco <80124925+PalawaJoko@users.noreply.github.com>
2023-11-29 19:10:17 -0300

    Codigo Final
    Creacion del header del modulo submenu

---
+++ SubMenu.h
@@ -0,0 +1,15 @@
+#ifndef SUBMENU_H
+#define SUBMENU_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 10
+#define EXIT 0
+
+void menuTemas(char Array[][MAX]);
+void menuDif(char Array[][MAX]);
+int subMenu();
+
+#endif
