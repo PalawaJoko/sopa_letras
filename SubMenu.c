603f525c18363a55c5cd188f88855e856c5ca32f
Marco <80124925+PalawaJoko@users.noreply.github.com>
2023-12-02 11:12:41 -0300

    Pulido de código
    Se pule el codigo, quedando lista la relacion Menu - SubMenu - header SubMenu

--- SubMenu.c
+++ SubMenu.c
@@ -1,35 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#define MAX 10
-int EXIT = 0;
-//A = [tema, dif]
-char Datos[2][MAX] = {"a", "b"};
+#include "SubMenu.h"
 
-void menuTemas(char Array[][MAX]){
+int menuTemas(char datos[2][1]){
 	char aux;
-	printf("------TEMAS------ \n");
-	printf("1. Animales \n2. Hogar \n3. Comida \n4. Volver\n");
-	printf("Escoge un tema: ");
-	scanf("%s", &aux);
-	switch(aux){
-		case '1':
-			strcpy(Array[0], "A");
-			break;
-		case '2':
-			strcpy(Array[0], "H");
-			break;
-		case '3':
-			strcpy(Array[0], "C");
-			break;
-		/*case 4: funcion anterior
-			menuPrincipal*/
-		default:
-			printf("Entrada incorrecta, Vuelva a intentarlo\n");
-			menuTemas(Datos);
-	}
+	do{
+		printf("------TEMAS------ \n");
+		printf("1. Animales \n2. Hogar \n3. Comida \n4. Volver\n");
+		printf("Escoge un tema: ");
+		scanf("%s", &aux);
+		switch(aux){
+			case '1':
+				strcpy(datos[0], "A");
+				break;
+			case '2':
+				strcpy(datos[0], "H");
+				break;
+			case '3':
+				strcpy(datos[0], "C");
+				break;
+			case '4': 
+				return 0;
+			default:
+				printf("Entrada incorrecta, Vuelva a intentarlo\n");
+			}
+	}while(aux!=4);
+	return 0;
 }
-void menuDif(char Array[][MAX]){
+void menuDif(char datos[2][1]){
 	char aux;
 	printf("------DIFICULTAD------ \n");
 	printf("1. Facil \n2. Media \n3. Dificil\n4. Volver\n");
@@ -37,25 +36,27 @@ void menuDif(char Array[][MAX]){
 	scanf("%s", &aux);
 	switch(aux){
 		case '1':
-			strcpy(Array[1], "F");
+			strcpy(datos[1], "F");
 			break;
 		case '2':
-			strcpy(Array[1], "M");
+			strcpy(datos[1], "M");
 			break;
 		case '3':
-			strcpy(Array[1], "D");
+			strcpy(datos[1], "D");
 			break;
 		case '4': 
-			menuTemas(Datos);
+			menuTemas(datos);
 		default:
 			printf("Entrada incorrecta, Vuelva a intentarlo\n");
-			menuDif(Datos);
+			menuDif(datos);
 	}
 }
 
 int subMenu(){
-	menuTemas(Datos);
-	menuDif(Datos);
-	ModuloMatriz(Datos);
-	return 0;
+	char datos[2][1];
+	int flujo;
+	flujo = menuTemas(datos);
+	menuDif(datos);
+	//ModuloMatriz(Datos);
+	return flujo;
 }
