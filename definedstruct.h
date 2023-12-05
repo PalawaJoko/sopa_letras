aed8f4ddbbfece108e0323597fdad29cbdfa1984
Marco <80124925+PalawaJoko@users.noreply.github.com>
2023-12-05 17:11:38 -0300

    Add files via upload

---
+++ definedstruct.h
@@ -0,0 +1,14 @@
+#ifndef DEFINEDSTRUCTS_H
+#define DEFINEDSTRUCTS_H
+
+//clase que define las estructuras mas utilizadas en el programa
+struct string{
+    char dato[60];
+};
+
+struct casilla{
+    char dato[60];
+    bool ocupado;
+};
+
+#endif
