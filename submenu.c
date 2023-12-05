669798e5a8f1d977f03e444d4339371f90d5b0ac
Marco <80124925+PalawaJoko@users.noreply.github.com>
2023-12-05 17:20:19 -0300

    CODIGO FINAL

--- submenu.c
+++ submenu.c
@@ -1,34 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include "SubMenu.h"
+#include "submenu.h"
+#include "modulomatriz.h"
 
-int menuTemas(char datos[2][1]){
+int menuTemas(char datos[][1]){
 	char aux;
-	do{
-		printf("------TEMAS------ \n");
-		printf("1. Animales \n2. Hogar \n3. Comida \n4. Volver\n");
-		printf("Escoge un tema: ");
-		scanf("%s", &aux);
-		switch(aux){
-			case '1':
-				strcpy(datos[0], "A");
-				break;
-			case '2':
-				strcpy(datos[0], "H");
-				break;
-			case '3':
-				strcpy(datos[0], "C");
-				break;
-			case '4': 
-				return 0;
-			default:
-				printf("Entrada incorrecta, Vuelva a intentarlo\n");
-			}
-	}while(aux!=4);
+	printf("------TEMAS------ \n");
+	printf("1. Animales \n2. Hogar \n3. Comida \n4. Volver\n");
+	printf("Escoge un tema: ");
+	scanf("%s", &aux);
+	switch(aux){
+		case '1':
+			strcpy(datos[0], "A");
+			break;
+		case '2':
+			strcpy(datos[0], "H");
+			break;
+		case '3':
+			strcpy(datos[0], "C");
+			break;
+		case '4': 
+			return 1;
+		default:
+			printf("Entrada incorrecta, Vuelva a intentarlo\n");
+			return menuTemas(datos);
+		}
 	return 0;
 }
-void menuDif(char datos[2][1]){
+void menuDif(char datos[][1]){
 	char aux;
 	printf("------DIFICULTAD------ \n");
 	printf("1. Facil \n2. Media \n3. Dificil\n4. Volver\n");
@@ -57,6 +57,8 @@ int subMenu(){
 	int flujo;
 	flujo = menuTemas(datos);
 	menuDif(datos);
-	//ModuloMatriz(Datos);
+	if(flujo!=1){
+		flujo = moduloMatriz(datos);
+	}
 	return flujo;
 }
