d2967a27c85d53067211b656331df7c8419321ff
Marco <80124925+PalawaJoko@users.noreply.github.com>
2023-12-05 17:11:03 -0300

    CODIGO FINAL

--- GenerarTexto.h
+++ GenerarTexto.h
@@ -5,15 +5,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "definedstruct.h"
 
-struct string{
-    char dato[60];
-};
-
-struct casilla{
-    char dato[60];
-    bool ocupado;
-};
+#define tamañoMatriz 1
 
 //rellena aleatoriamente un arreglo de ints
 void rellenarAleatorios(int *arreglo,int tamaño, int tope);
@@ -22,19 +16,19 @@ int largoPalabra(char palabra[60]);
 //funcion que seleccionar, de for,a aleatoria, palabras para la sopa de letras
 void seleccionarPalabras(struct string escogidas[], char ruta[100], int numeroPalabras, int tope);
 //colocar palabras en 4 direcciones
-bool ponerHorizontalD(char recibido[60],struct casilla sopa[TAMAÑO_MATRIZ][TAMAÑO_MATRIZ], int fila, int columna);
-bool ponerHorizontalI(char recibido[60],struct casilla sopa[TAMAÑO_MATRIZ][TAMAÑO_MATRIZ], int fila, int columna);
-bool ponerVerticalInf(char recibido[60],struct casilla sopa[][TAMAÑO_MATRIZ], int fila, int columna);
-bool ponerVerticalSup(char recibido[60],struct casilla sopa[][TAMAÑO_MATRIZ], int fila, int columna);
+bool ponerHorizontalD(char recibido[60],struct casilla sopa[tamañoMatriz][tamañoMatriz], int fila, int columna);
+bool ponerHorizontalI(char recibido[60],struct casilla sopa[tamañoMatriz][tamañoMatriz], int fila, int columna);
+bool ponerVerticalInf(char recibido[60],struct casilla sopa[][tamañoMatriz], int fila, int columna);
+bool ponerVerticalSup(char recibido[60],struct casilla sopa[][tamañoMatriz], int fila, int columna);
 //funcion que devuelve true si se pudo poner la palabra en alguna de las 4 direcciones
-bool ponerPalabra(char recibido[60],struct casilla sopa[][TAMAÑO_MATRIZ], int fila, int columna);
+bool ponerPalabra(char recibido[60],struct casilla sopa[][tamañoMatriz], int fila, int columna);
 //funcion que coloca palabras en la sopa, haciendo uso de ponerPalabra como recurso
-void colocarPalabras(struct string escogidas[],struct casilla sopa[][TAMAÑO_MATRIZ],int numeroPalabras);
+void colocarPalabras(struct string escogidas[],struct casilla sopa[][tamañoMatriz],int numeroPalabras);
 //funcion que rellena la sopa con letras aleatorias
-void rellenarLetrasAleatorias(struct casilla sopa[][TAMAÑO_MATRIZ]);
+void rellenarLetrasAleatorias(struct casilla sopa[][tamañoMatriz]);
 //funcion que reemplaza las palabras repetidas por otras
 void arreglarEscogidas(struct string escogidas[],char tema, int numeroPalabras);
 
-void generarTexto(char datos[][1],struct casilla sopa[TAMAÑO_MATRIZ][TAMAÑO_MATRIZ],struct string escogidas[numeroPalabras]);
+void GenerarTexto(char datos[][1],struct casilla sopa[tamañoMatriz][tamañoMatriz],struct string escogidas[tamañoMatriz-1]);
 
 #endif
